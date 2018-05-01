f8817c1c79d904c13441c69c039acec2e75b8080
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-01 05:56:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function lowerCaseDrivers(array) {
-  array.map(function(driver){return Object.assign({}, driver, driver.toLowerCase())
+  array.map(function(driver){return Object.assign({}, array, driver.toLowerCase())
 }
 
 function nameToAttributes(){
